18320bb5acc99889f654b3b305f05d66aec9c6d2
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-02-04 19:07:05 +0100

    test function fetching lang & long

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("async function getWeather() {\n    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=55.6761&lon=12.5683&appid=98cf7b78cb1f92c6762b5df863981c84', {mode: 'cors'});\n    const weatherData = await response.json();\n    console.log(weatherData);\n  }\n\n  getWeather();\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("async function getWeather() {\n    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=55.6761&lon=12.5683&appid=98cf7b78cb1f92c6762b5df863981c84', {mode: 'cors'});\n    const weatherData = await response.json();\n    const cityName = weatherData.name;\n    console.log(cityName);\n    console.log(weatherData);\n  }\n\n  getWeather();\n\n\nasync function getCoordinates() {\n    const copenhagen = \"Copenhagen\";\n    const firstPart = \"http://api.openweathermap.org/geo/1.0/direct?q=\";\n    const lastPart = \"&limit=&appid=98cf7b78cb1f92c6762b5df863981c84\"\n    const combined = firstPart + copenhagen + lastPart\n    const response = await fetch(combined, {mode: 'cors'});\n    const Data = await response.json();          \n    const langtitude = Data[0].lat;\n    const longtitude = Data[0].lon;\n    console.log(Data);\n    console.log(langtitude);\n    console.log(longtitude);\n  }\n\n  getCoordinates();\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
