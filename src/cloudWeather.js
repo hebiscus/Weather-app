81b7404aee0355b9f470d0d93f68296d6020cb2a
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-02-08 19:15:10 +0100

    containers for rendered values

--- src/cloudWeather.js
+++ src/cloudWeather.js
@@ -8,11 +8,24 @@ export const cloudWeather = {
         content.classList.add("cloud-grid");
         
         const cityName = weatherData.city.name;
-        const weatherTemperature = weatherData.list[0].main.temp;
+        const weatherTemperature = Math.ceil(weatherData.list[0].main.temp);
         const weatherDescription = weatherData.list[0].weather[0].description;
         const humidityStat = weatherData.list[0].main.humidity
         const rainChance = weatherData.list[0].pop
         const windSpeed = weatherData.list[0].wind.speed;
+
+        const nameP = document.createElement("p");
+        nameP.innerText = cityName;
+        const temperatureP = document.createElement("p");
+        temperatureP.innerText = `${weatherTemperature} °C`
+        const descriptionP = document.createElement("p");
+        descriptionP.innerText = weatherDescription;
+        const humidityP = document.createElement("p");
+        humidityP.innerText = humidityStat;
+        const rainP = document.createElement("p");
+        rainP.innerText = rainChance;
+        const windP = document.createElement("p");
+        windP.innerText = windSpeed;
        
         const inputSearch = document.createElement("input");
         inputSearch.setAttribute("type", "search");
@@ -24,8 +37,8 @@ export const cloudWeather = {
         const secondaryBox = document.createElement("div");
         secondaryBox.classList.add("secondary-cloud");
 
-        mainBox.append(weatherDescription, cityName, weatherTemperature);
-        secondaryBox.append(humidityStat, rainChance, windSpeed);
+        mainBox.append(descriptionP, nameP, temperatureP);
+        secondaryBox.append(humidityP, rainP, windP);
         content.append(inputSearch, mainBox, secondaryBox);
         console.log(cityName);
         renderView();
